4344cd7d098de71defe087f2a374fb8c2d5b2fbb
AlexKharenko <alexey16052002@gmail.com>
2021-07-07 23:41:00 +0300

    Add Activity for Edit and Delete Pop Up and Add task_id value

--- src/store/modules/pop_ups.js
+++ src/store/modules/pop_ups.js
@@ -1,25 +1,43 @@
 const state = {
   add_task_pop_up_active: false,
-  add_task_called_from: "todo",
+  edit_task_pop_up_active: false,
+  task_id: undefined,
+  delete_task_pop_up_active: false,
+  pop_up_task_called_from: "todo",
 };
 
 const getters = {
   isAddTaskPopUpActive: (state) => state.add_task_pop_up_active,
-  getAddTaskCalledFrom: (state) => state.add_task_called_from,
+  isEditTaskPopUpActive: (state) => state.edit_task_pop_up_active,
+  isDeleteTaskPopUpActive: (state) => state.delete_task_pop_up_active,
+  getTaskId: (state) => state.task_id,
+  popUpTaskCalledFrom: (state) => state.pop_up_task_called_from,
 };
 
 const actions = {
   changeAddTaskPopUpStatus({ commit }, status) {
     commit("setAddTaskPopUpStatus", status);
   },
-  changeAddTaskCalledFrom({ commit }, id) {
-    commit("setAddTaskCalledFrom", id);
+  changeEditTaskPopUpStatus({ commit }, status) {
+    commit("setEditTaskPopUpStatus", status);
+  },
+  changeDeleteTaskPopUpStatus({ commit }, status) {
+    commit("setDeleteTaskPopUpStatus", status);
+  },
+  changeTaskId({ commit }, id) {
+    commit("setTaskId", id);
+  },
+  changePopUpTaskCalledFrom({ commit }, id) {
+    commit("setPopUpTaskCalledFrom", id);
   },
 };
 
 const mutations = {
   setAddTaskPopUpStatus: (state, data) => (state.add_task_pop_up_active = data),
-  setAddTaskCalledFrom: (state, data) => (state.add_task_called_from = data),
+  setEditTaskPopUpStatus: (state, data) => (state.edit_task_pop_up_active = data),
+  setDeleteTaskPopUpStatus: (state, data) => (state.delete_task_pop_up_active = data),
+  setTaskId: (state, data) => (state.task_id = data),
+  setPopUpTaskCalledFrom: (state, data) => (state.pop_up_task_called_from = data),
 };
 
 export default {
